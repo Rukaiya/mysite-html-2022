8c286e94ca9d00c9b4f3b053f1c08a7b649148f7
Rukaiya <rukaiyaamin36@gmail.com>
2022-10-10 18:10:35 +0600

    t-4

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponse
 from django.views import generic
-from .models import Question, Choice
-
+from polls.models import Question, Choice
 
 # Create your views here.
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')
-    context = {latest_questions: latest_questions}
+    context = {'latest_questions': latest_questions}
     return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
@@ -21,4 +21,16 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+
