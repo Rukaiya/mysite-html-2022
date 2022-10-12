5b771690edd04a7cd51dbcbda9c0bc08f541f215
Rukaiya Amin <rukaiyaamin36@gmail.com>
2022-10-12 08:17:34 +0600

    t-4: results.html

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404, HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views import generic
 from polls.models import Question, Choice
+
 
 # Create your views here.
 def index(request):
@@ -16,8 +18,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -32,5 +34,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-
-
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
